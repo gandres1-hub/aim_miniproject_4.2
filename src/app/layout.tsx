bcddf367741825400c7aefc80b45891bf4ec4891
Gene Andres <gene.andres@gmail.com>
2026-07-28 06:45:36 +1000

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { RoleProvider } from "@/lib/roleContext";
 import { RoleToggle } from "@/components/RoleToggle";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
             <RoleToggle />
           </header>
           {children}
+          
         </RoleProvider>
       </body>
     </html>
